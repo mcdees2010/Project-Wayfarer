36bdf9b689e37a6bf6aaedbcad77bd2e6ae29694
Maria Dees <mcdees2010@gmail.com>
2018-10-06 14:21:22 -0700

    added sign-up requirements for an email and password if it is not filled in correctly, flashes email is already taken

--- config/passport.js
+++ config/passport.js
@@ -11,3 +11,18 @@ passport.deserializeUser((id, done) => {
         done(err, user);
     });
 });
+
+passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, (req, email, password, done) => {
+    User.findOne({ email }, (err, user) => {
+        if (err) return done(err);
+        if (user) return done(null, false, req.flash('signupMessage', "Email already used." ));
+        User.create(req.body, (err, newUser) => {
+            if (err) return console.log(err);
+            return done(null, newUser, null);
+        })
+    })
+}));
