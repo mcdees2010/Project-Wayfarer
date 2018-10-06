f5b861d5871c221eb2bfcf4c007c27fb966b1cec
Maria Dees <mcdees2010@gmail.com>
2018-10-06 14:18:48 -0700

    created a serialize and deserialize user, giving a session key while logged in

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,13 @@
 const passport = require('passport'),
-      LocalStrategy = require('passport-local').Strategy;
+      LocalStrategy = require('passport-local').Strategy,
+      User = require('../models/User');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+    
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
